1acf1a8f047b7001768b334be4001e19e724e1b1
enofeb <eneszor95@gmail.com>
2020-01-04 15:31:51 +0100

    just empty commit

--- app/src/main/java/com/cnkaptan/tmonsterswiki/remote/api/MonstersApi.kt
+++ app/src/main/java/com/cnkaptan/tmonsterswiki/remote/api/MonstersApi.kt
@@ -1,10 +1,8 @@
 package com.cnkaptan.tmonsterswiki.remote.api
 
-import com.cnkaptan.tmonsterswiki.data.local.entity.MonsterLevelEntity
 import com.cnkaptan.tmonsterswiki.remote.model.MonsterMainResponse
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface MonstersApi {
     @GET("main_data")
