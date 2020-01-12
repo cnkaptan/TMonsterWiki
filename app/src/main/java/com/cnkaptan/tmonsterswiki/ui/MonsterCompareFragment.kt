9ddb7adbc4649889d3f9185d66542f0d5a8dbd5d
enofeb <eneszor95@gmail.com>
2020-01-12 23:12:53 +0100

    compare page ui updated

--- app/src/main/java/com/cnkaptan/tmonsterswiki/ui/MonsterCompareFragment.kt
+++ app/src/main/java/com/cnkaptan/tmonsterswiki/ui/MonsterCompareFragment.kt
@@ -1,6 +1,8 @@
 package com.cnkaptan.tmonsterswiki.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,28 +13,29 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.addisonelliott.segmentedbutton.SegmentedButtonGroup
 import com.cnkaptan.tmonsterswiki.AppController
 import com.cnkaptan.tmonsterswiki.R
 import com.cnkaptan.tmonsterswiki.data.local.entity.MonsterEntity
-import com.cnkaptan.tmonsterswiki.databinding.ActivityMonsterDexBinding
+import com.cnkaptan.tmonsterswiki.databinding.ActivityMonsterCompareBinding
 import com.cnkaptan.tmonsterswiki.ui.adapter.MonsterCompareAdapter
 import com.cnkaptan.tmonsterswiki.ui.viewmodel.MonsterCompareViewModel
+import com.cnkaptan.tmonsterswiki.utils.Constants
+import com.tiper.MaterialSpinner
+import kotlinx.android.synthetic.main.activity_monster_compare.*
 import javax.inject.Inject
 
-
-
-class MonsterCompareFragment : Fragment(), AdapterView.OnItemSelectedListener,SegmentedButtonGroup.OnPositionChangedListener {
+class MonsterCompareFragment : Fragment(),RadioGroup.OnCheckedChangeListener {
 
 
     @Inject
     internal lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    private lateinit var binding: ActivityMonsterDexBinding
+    private lateinit var binding: ActivityMonsterCompareBinding
     private lateinit var monsterCompareViewModel: MonsterCompareViewModel
     private lateinit var monsterCompareAdapter: MonsterCompareAdapter
 
     private var selectedLevel: Int = 0
+    private var selectedRadio: Int = 0
     private var sortListByAsc: List<MonsterEntity> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,20 +48,20 @@ class MonsterCompareFragment : Fragment(), AdapterView.OnItemSelectedListener,Se
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.activity_monster_dex,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.activity_monster_compare,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViewModel()
+        binding.radioButtonGroup.setOnCheckedChangeListener(this)
         initSpinner()
-
-        binding.segmentedButtonGroup.onPositionChangedListener = this
+        initTextFont()
     }
 
     private fun initView(monsterEntity: List<MonsterEntity>) {
-        binding.spnMonsterDex.onItemSelectedListener = this
+        binding.spnMonsterDex.onItemSelectedListener = listener
         monsterCompareAdapter = MonsterCompareAdapter(requireContext())
         binding.rvMonsterDexList.apply {
             layoutManager = LinearLayoutManager(context)
@@ -74,6 +77,7 @@ class MonsterCompareFragment : Fragment(), AdapterView.OnItemSelectedListener,Se
         monsterCompareViewModel.getMonsterWithLevels().observe(this, Observer {
             initView(it)
             sortListByAsc = it
+            rbHealth.isChecked=true
         })
     }
 
@@ -84,32 +88,73 @@ class MonsterCompareFragment : Fragment(), AdapterView.OnItemSelectedListener,Se
         binding.spnMonsterDex.adapter = aa
     }
 
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        if(p1!!.equals("Select Level")){
-            monsterCompareAdapter.updateLevel(0)
-            selectedLevel=0
-        }else{
-            monsterCompareAdapter.updateLevel(p2)
-            selectedLevel = p2
+    override fun onCheckedChanged(p0: RadioGroup?, id: Int) {
+        when (id) {
+            R.id.rbHealth -> {
+                selectedRadio = 0
+                sortHealthList(selectedLevel)
+            }
+            R.id.rbDamage -> {
+                selectedRadio = 1
+                sortDamageList(selectedLevel)
+            }
+            R.id.rbSpeed ->{
+                selectedRadio = 2
+                sortSpeedList(selectedLevel)
+            }
         }
     }
 
-    override fun onNothingSelected(p0: AdapterView<*>?) {
+    private fun sortHealthList(level: Int) {
+        val sortedListHp = sortListByAsc.sortedWith(compareBy { it.levels.get(level).hp })
+            .reversed()
+        monsterCompareAdapter.updateMonster(sortedListHp)
     }
 
-    override fun onPositionChanged(checkedId: Int) {
-        when (checkedId) {
-            0 -> {
-                val sortedListHp = sortListByAsc.sortedWith(compareBy { it.levels.get(selectedLevel).hp })
-                monsterCompareAdapter.updateMonster(sortedListHp)
+    private fun sortDamageList(level: Int) {
+        val sortedListDmg = sortListByAsc.sortedWith(compareBy { it.levels.get(level).dmg })
+            .reversed()
+        monsterCompareAdapter.updateMonster(sortedListDmg)
+    }
+
+    private fun sortSpeedList(level: Int) {
+        val sortedListSpeed = sortListByAsc.sortedWith(compareBy { it.levels.get(level).speed })
+            .reversed()
+        monsterCompareAdapter.updateMonster(sortedListSpeed)
+    }
+
+    private fun initTextFont(){
+        val typeFace = Typeface.createFromAsset(requireActivity().assets, Constants.MONSTERNAMEFONT)
+        binding.spnMonsterDex.typeface=typeFace
+    }
+
+    private val listener by lazy {
+        object : MaterialSpinner.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: MaterialSpinner,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.e("MaterialSpinner", "onItemSelected parent=${parent.id}, position=$position")
+                monsterCompareAdapter.updateLevel(position)
+
+                when (selectedRadio) {
+                    0 -> {
+                        sortHealthList(position)
+                    }
+                    1 -> {
+                        sortDamageList(position)
+                    }
+                    2 -> {
+                        sortSpeedList(position)
+                    }
+                }
+                selectedLevel = position
+                parent.focusSearch(View.FOCUS_UP)?.requestFocus()
             }
-            1 -> {
-                val sortedListDmg = sortListByAsc.sortedWith(compareBy { it.levels.get(selectedLevel).dmg })
-                monsterCompareAdapter.updateMonster(sortedListDmg)
-            }
-            else -> {
-                val sortedListMove = sortListByAsc.sortedWith(compareBy { it.levels.get(selectedLevel).speed })
-                monsterCompareAdapter.updateMonster(sortedListMove)
+
+            override fun onNothingSelected(parent: MaterialSpinner) {
             }
         }
     }
--- app/src/main/java/com/cnkaptan/tmonsterswiki/ui/adapter/MonsterCompareAdapter.kt
+++ app/src/main/java/com/cnkaptan/tmonsterswiki/ui/adapter/MonsterCompareAdapter.kt
@@ -1,6 +1,7 @@
 package com.cnkaptan.tmonsterswiki.ui.adapter
 
 import android.content.Context
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cnkaptan.tmonsterswiki.R
 import com.cnkaptan.tmonsterswiki.data.local.entity.MonsterEntity
 import com.cnkaptan.tmonsterswiki.di.module.ApiModule
+import com.cnkaptan.tmonsterswiki.utils.Constants
 import com.squareup.picasso.Picasso
 
 class MonsterCompareAdapter(
@@ -22,7 +24,7 @@ class MonsterCompareAdapter(
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonsterViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_monster_dex, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_monster_compare, parent, false)
         return MonsterViewHolder(itemView)
     }
 
@@ -52,6 +54,18 @@ class MonsterCompareAdapter(
                 .load(drawableId)
                 .placeholder(R.drawable.splash_logo)
                 .into(holder.ivMonster)
+
+        val typeFace = Typeface.createFromAsset(context.assets, Constants.MONSTERNAMEFONT)
+
+        holder.tvMonsterName.typeface = typeFace
+        holder.tvMonsterMove.typeface = typeFace
+        holder.tvMonsterSpeed.typeface = typeFace
+        holder.tvMonsterHit.typeface = typeFace
+        holder.tvMonsterCrit.typeface = typeFace
+        holder.tvMonsterPhyDef.typeface = typeFace
+        holder.tvMonsterMagDef.typeface = typeFace
+        holder.tvMonsterHealth.typeface = typeFace
+        holder.tvMonsterDamage.typeface = typeFace
     }
 
     fun updateLevel(getLevel: Int) {
@@ -65,11 +79,18 @@ class MonsterCompareAdapter(
         notifyDataSetChanged()
     }
 
+
+
     class MonsterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvMonsterName: TextView = itemView.findViewById(R.id.tvMonsterName)
         val tvMonsterHealth: TextView = itemView.findViewById(R.id.tvMonsterHealth)
         val tvMonsterDamage: TextView = itemView.findViewById(R.id.tvMonsterDamage)
-        val tvMonsterMove: TextView = itemView.findViewById(R.id.tvMonsterSpeed)
+        val tvMonsterMove: TextView = itemView.findViewById(R.id.tvMonsterMove)
+        val tvMonsterSpeed: TextView = itemView.findViewById(R.id.tvMonsterSpeed)
+        val tvMonsterHit: TextView = itemView.findViewById(R.id.tvMonsterHit)
+        val tvMonsterCrit: TextView = itemView.findViewById(R.id.tvMonsterCrit)
+        val tvMonsterPhyDef: TextView = itemView.findViewById(R.id.tvMonsterPhyDef)
+        val tvMonsterMagDef: TextView = itemView.findViewById(R.id.tvMonsterMagDef)
         val ivMonster: ImageView = itemView.findViewById(R.id.ivMonsterDex)
         val bigFrame: RelativeLayout = itemView.findViewById(R.id.rvBigFrame)
         val smallFrame: RelativeLayout = itemView.findViewById(R.id.rvSmallFrame)
